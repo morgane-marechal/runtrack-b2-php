33af5b9ec88f36c2ad2b3dc2f4e13573aa312449
morgane-marechal <morgane.marechal@laplateforme.io>
2023-09-06 10:48:39 +0200

    add job-02

---
+++ jour-01/job-02/index.php
@@ -0,0 +1,24 @@
+<?php
+function my_str_reverse(string $string) : string{
+    $str_length=0;
+   // $occurences_number=0;
+    $newArray=[];
+    $newString="";
+    while(isset($string[$str_length])){
+        $str_length++;        
+    }
+
+    for ($j= $str_length; $j >= 0; $j--) {
+      array_push($newArray, $string[$j]);
+    }
+    //var_dump($newArray);
+
+    for($k=0; $k<=$str_length; $k++){
+        $newString=$newString.$newArray[$k];
+    }
+    echo $newString;
+}
+
+echo my_str_reverse('hello World');
+
+?>
